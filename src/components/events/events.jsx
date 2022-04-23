61c0c3f8dbd70e94adaabfe24eb30c22a94c6a9d
Psalmseen <55616929+Psalmseen@users.noreply.github.com>
2022-04-23 21:53:13 +0100

    added the events page

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Event = () => (
-  <section name="events" className="events">
-    {' '}
-  </section>
-);
+import img from '../../assets/event.jpg';
+
+const Event = () => {
+  const [event, setEvent] = useState([]);
+
+  return (
+    <section name="events" className="events">
+      <div
+        className="event-header-background"
+        style={{
+          backgroundImage: `url(${img})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      >
+        <div className="events__outline"> Events </div>
+        <div className="events__title"> Upcoming Events</div>
+      </div>
+      {event.length > 0 ? (
+        'event'
+      ) : (
+        <div className="noEvent"> No upcoming event</div>
+      )}
+    </section>
+  );
+};
 
 export default Event;
